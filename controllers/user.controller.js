f5fa68a53c715e78f4e4b4db82a9f708e1545310
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-05 23:45:41 +0530

    auth basics

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,52 @@
+import { User } from '../model/UserSchame.model.js'
 const registerUser = async (req , res) => {
-    res.send("Hi")
+    // Get user data
+    const { name , email , password} = req.body;
+    console.log(req.body)
+    // validate
+    if(!name || !email || !password) {
+        return res.status(400).json({
+            success: false,
+            message: "Invalid data"
+        })
+    }
+
+    // console.log(name, email, password);
+    // existes or not 
+   try{
+    const userExists = await User.findOne({ email })
+    if(userExists) {
+        return res.status(401).json({
+            success: false,
+            message: "User already exixts"
+        })
+    }
+    // create user in db
+    const response = await User.create({
+        name,
+        email,
+        password,
+    })
+    
+    // create a varification token 
+ 
+    // save varification token in DB 
+ 
+    // send token as email to 
+ 
+    // send success status to Client/User
+    
+   }catch(error) {
+        console.log(error.message)
+        return res.status(501).json({
+            success: false,
+            message: "Failed to register the user, some internal issues on server",
+            error: error.message,
+        })
+   }
+    
+
+
 };
 
 export { registerUser };
