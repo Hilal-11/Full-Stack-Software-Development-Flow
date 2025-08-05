0538cdd87aae474d1aef25cb145a10cf7aab92a3
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-05 22:42:35 +0530

    routs

---
+++ routes/user.routes.js
@@ -0,0 +1,8 @@
+import express from 'express'
+const router = express.Router();
+
+import { registerUser } from '../controllers/user.controller.js'
+
+router.get('/register' , registerUser)
+
+export default router
