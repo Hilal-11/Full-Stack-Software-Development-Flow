858323646b38db57221080a22086b958821fd1e1
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-07 17:56:18 +0530

    auth

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,12 @@
 import express from 'express'
 const router = express.Router();
 
-import { registerUser, varifyUser } from '../controllers/user.controller.js'
+import { login, registerUser, varifyUser } from '../controllers/user.controller.js'
 
 router.post('/register' , registerUser)
 router.get('/varify/:token' , varifyUser)
+router.get('/login' , login)
+
 
 
 export default router
