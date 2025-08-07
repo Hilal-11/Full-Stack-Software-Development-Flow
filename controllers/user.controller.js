3261a443346d6a122c157394dd649740407fea5e
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-07 16:28:15 +0530

    regester and varify

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,7 +2,10 @@ import { User } from '../model/UserSchame.model.js'
 import crypto from 'crypto'
 import nodemailer from 'nodemailer'
 import dotenv from 'dotenv'
+import bcrypt from 'bcryptjs'
 dotenv.config()
+
+
 const registerUser = async (req , res) => {
     // Get user data
     const { name , email , password} = req.body;
@@ -26,10 +29,15 @@ const registerUser = async (req , res) => {
         })
     }
     // create user in db
+    // encrypt the password
+    const salt_round = await bcrypt.genSalt(10);
+    const hash_password = await bcrypt.hash(password , salt_round)
+
+
     const user = await User.create({
         name,
         email,
-        password,
+        password: hash_password,
     })
 
     if(!user) {
@@ -90,4 +98,50 @@ const registerUser = async (req , res) => {
 
 };
 
-export { registerUser };
+
+const varifyUser = async (req , res) => {
+    // get token from url
+    // validate isToken oe not
+    // find user based on token
+    // compare token is valid or not 
+    // if valid ----> set isVarifird to [true]
+    // remove varification token now 
+    // save 
+    // send success response 
+    try{
+        const { token } = req.params;
+        if(!token) {
+            return res.status(400).json({
+                success: false,
+                message: "token is missing"
+            })
+        }
+
+        const user = await User.findOne({ varificationToken: token });
+        if(!user) {
+             return res.status(400).json({
+                success: false,
+                message: "Invalid token"
+            })
+        }
+
+        user.isVarified = true
+        user.varificationToken = undefined
+
+        await user.save();
+
+        res.status(200).json({
+            success: true,
+            message: "user varification successful"
+        })
+
+    }catch(error) {
+        console.log(error.message);
+        res.status().json({
+            success: false,
+            message: "Failed to varify the user",
+            error: error.message
+        })
+    }
+}
+export { registerUser , varifyUser };
