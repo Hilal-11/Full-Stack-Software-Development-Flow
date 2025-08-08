f8bb1c333043b331a2c8e7d987fb5556d2c9337d
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-08 17:29:14 +0530

    done

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,15 @@
 import express from 'express'
 const router = express.Router();
 
-import { login, registerUser, varifyUser } from '../controllers/user.controller.js'
+import { login, registerUser, varifyUser , userProfile, logout } from '../controllers/user.controller.js'
+import { isLoggedIn } from '../middleware/auth.middleware.js';
 
 router.post('/register' , registerUser)
 router.get('/varify/:token' , varifyUser)
 router.post('/login' , login)
+
+router.get('/profile' , isLoggedIn , userProfile)
+router.get('/logout' , isLoggedIn , logout)
 
 
 
